a424667cb67827c5a58dd5956f36224628211f01
choiwono <treasure_b@naver.com>
2019-02-06 16:55:19 +0900

    #19.02.06 함수형 자바스크립트 연습 1-40까지

--- script03.js
+++ script03.js
@@ -85,3 +85,161 @@ console.log(
 console.log(
     find(users, bmatch({ name: 'ID3', age:32 }))
 );
+
+function findIndex(list, predicate) {
+    for ( var i = 0, len = list.length; i < len; i++) {
+        if(predicate(list[i])) return i;
+    }
+    return -1;
+}
+
+console.log(findIndex(users, bmatch({ age:27 })));
+
+_.map = function(list,iteratee) {
+    var new_list = [];
+    for (var i = 0, len=list.length; i<len; i++) {
+        new_list.push(iteratee(list[i],i,list));
+    }
+    return new_list;
+};
+
+_.filter = function(list, predicate) {
+    var new_list = [];
+    for (var i = 0, len = list.length; i < len; i++) {
+        if(predicate(list[i], i, list)) new_list.push(list[i]);
+    }
+    return new_list;
+};
+
+_.find = function(list,predicate) {
+    for(var i = 0, len = list.length; i < len; i++) {
+        if (predicate(list[i],i,list)) return list[i];
+    }
+};
+
+_.findIndex = function(list,predicate) {
+    for (var i = 0, len = list.length; i < len; i++) {
+        if(predicate(list[i], i, list)) return i;
+    }
+    return -1;
+};
+
+console.log(_.filter([1,2,3,4], function(val,idx) {
+    return idx > 0; })
+);
+
+console.log(_.filter([1,2,3,4], function(val,idx) {
+    return idx % 2 == 0;    
+}));
+
+// 1-31
+_.identity = function(v) { return v; }
+var a = 10;
+console.log(_.identity(a));
+
+// 1-32
+console.log(_.filter([true, 0, 10, 'a', null],_.identity)); // identity를 통해서 잘못된 값을 필터링했다.
+// console = true,10,'a'
+
+_.falsy = function(v) { return !v; };
+_.truthy = function(v) { return !!v; };
+
+// 1-33
+
+_.some = function(list) {
+    return !!_.find(list,_.identity);
+};
+
+_.every = function(list) {
+    return _.filter(list, _.identity).length == list.length;
+};
+
+console.log(_.some([0,null,2]));
+console.log(_.some([0,null,false]));
+
+console.log(_.every(0, null, 2)); // 모든 리스트의 값을 체크
+console.log(_.every([{},true,2]));
+
+// 1-34
+
+function not(v) { return !v; }
+function beq(a) {
+    return function(b) {
+        return a === b;
+    }
+}
+
+// 1-35
+
+_.some = function(list) {
+    return !!_.find(list, _.identity);
+};
+
+_.every = function(list) {
+    return beq(-1)(_.findIndex(list,not));
+};
+
+console.log(_.some([0,null,2]));
+console.log(_.some([0,null,false]));
+console.log(_.every([0,null,2]));
+console.log(_.every([{},true,2]));
+
+function positive(list) {
+    return _.find(list, _.identity);
+}
+
+function negativeIndex(list) {
+    return _.findIndex(list, not);
+}
+
+/*_.some = function(list) {
+    return not(not(positive(list)));
+}
+
+_.every = function(list) {
+    return beq(-1)(negativeIndex(list));
+}*/
+
+console.log(not(positive([0,null,2])));
+
+console.log(_.some([0,null,2]));
+console.log(_.some([0,null,false]));
+console.log(_.every([0,null,2]));
+console.log(_.every([{},true,2]));
+
+// 1-37
+
+_.compose = function() {
+    var args = arguments;
+    var start = args.length - 1;
+    return function() {
+        var i = start;
+        var result = args[start].apply(this, arguments);
+        while (i--) result = args[i].call(this,result);
+        return result;
+    };
+};
+
+var greet = function(name) { return "hi: " + name };
+var exclaim = function(statement) { return statement.toUpperCase() + "!"; };
+var welcome = _.compose(greet, exclaim);
+console.log(welcome("moe"));
+
+_.some = _.compose(not, not, positive);
+_.every = _.compose(beq(-1), negativeIndex);
+
+// 1-39
+
+function f1() {}
+var a = typeof f1 == 'function' ? f1 : function() {};
+
+function f2() {
+    return function() {};
+}
+
+console.log((function(a,b){ return a + b; })(10,5));
+
+function callAndAdd(a,b) {
+    return a() + b();
+}
+callAndAdd(function() { return 10; }, function() { return 5; });
